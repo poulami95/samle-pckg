b376fef7b6ecb593ee3977396c8daa5a07e03021
poulami95 <mamai.babai63@gmail.com>
2019-06-18 11:59:24 +0530

    Second time commit

---
+++ example.js
@@ -0,0 +1,3 @@
+var isNullorEmpty = require('smpl-pckg-for-test');
+console.log(isNullorEmpty(" "));
+console.log(isNullorEmpty("Hello world"));
---
+++ index.js
@@ -0,0 +1,4 @@
+ function isNullorEmpty(input){
+	return(input === null|| input === undefined || === ' ');
+ }
+module.exports = isNullorEmpty;
